be4a5a2d956b7b035523188c2e6631a11bcfde06
Jamie Flynt <Jamflynt@gmail.com>
2026-02-18 13:59:25 -0500

    More UI fun

--- src/assets/js/testimonials.js
+++ src/assets/js/testimonials.js
@@ -1,9 +1,30 @@
 (function () {
-  var quotes = document.querySelectorAll(".testimonial-rotator blockquote");
+  var rotator = document.querySelector(".testimonial-rotator");
+  if (!rotator) return;
+
+  var quotes = rotator.querySelectorAll("blockquote");
   if (!quotes.length) return;
 
   var current = 0;
   quotes[0].classList.add("active");
+
+  function setHeight() {
+    var max = 0;
+    for (var i = 0; i < quotes.length; i++) {
+      quotes[i].style.position = "relative";
+      quotes[i].style.visibility = "hidden";
+      quotes[i].style.display = "block";
+      var h = quotes[i].offsetHeight;
+      if (h > max) max = h;
+      quotes[i].style.position = "";
+      quotes[i].style.visibility = "";
+      quotes[i].style.display = "";
+    }
+    rotator.style.height = max + "px";
+  }
+
+  setHeight();
+  window.addEventListener("resize", setHeight);
 
   setInterval(function () {
     quotes[current].classList.remove("active");
